52176fd43237f87835911422cc51c017fa0e8748
LoliE1ON <coreo2012@mail.ru>
2019-01-29 20:22:03 +0200

    Update webanalytic.js

--- webanalytic.js
+++ webanalytic.js
@@ -1,3 +1,13 @@
+// ==UserScript==
+// @name         VRChat Web Anal
+// @namespace    /
+// @version      /
+// @description  /
+// @author       Loli e1on
+// @match        https://vrchat.net/*
+// @grant        https://vrchat.net/*
+// ==/UserScript==
+
 /*
 * @name VRChat Web Analytic
 * @author Loli E1ON
@@ -16,33 +26,33 @@ var cookieAuth = getCookie("auth");
 var xhr = new XMLHttpRequest();
 
 if (cookieAuth != 'undefined') {
-	
+
 	// дохуя кода котороый я дето спиздил
-	history.pushState = ( f => function pushState(){ 
-	var ret = f.apply(this, arguments); 
-	window.dispatchEvent(new Event('pushState')); 
-	window.dispatchEvent(new Event('locationchange')); 
-	return ret; 
-	})(history.pushState); 
-	history.replaceState = ( f => function replaceState(){ 
-	var ret = f.apply(this, arguments); 
-	window.dispatchEvent(new Event('replaceState')); 
-	window.dispatchEvent(new Event('locationchange')); 
-	return ret; 
-	})(history.replaceState); 
-	window.addEventListener('popstate',()=>{ 
-	window.dispatchEvent(new Event('locationchange')) 
+	history.pushState = ( f => function pushState(){
+	var ret = f.apply(this, arguments);
+	window.dispatchEvent(new Event('pushState'));
+	window.dispatchEvent(new Event('locationchange'));
+	return ret;
+	})(history.pushState);
+	history.replaceState = ( f => function replaceState(){
+	var ret = f.apply(this, arguments);
+	window.dispatchEvent(new Event('replaceState'));
+	window.dispatchEvent(new Event('locationchange'));
+	return ret;
+	})(history.replaceState);
+	window.addEventListener('popstate',()=>{
+	window.dispatchEvent(new Event('locationchange'))
 	});
-	
+
 	// Смена url
-	window.addEventListener('locationchange', function(){ 
-	
+	window.addEventListener('locationchange', function(){
+
 		var path = location.pathname.split('/');
-		
+
 		// world card
 		if ((typeof path[2] !== "undefined") && (path[2] == 'world')) {
 			if ((typeof path[3] !== "undefined")) {
-				
+
 				var worldData = {};
 				// get world info
 				xhr.open("GET", "/api/1/worlds/"+path[3], true);
@@ -51,10 +61,10 @@ if (cookieAuth != 'undefined') {
 					getAllUsers(worldData);
 				}
 				xhr.send(null);
-	
+
 			}
 		}
-		
+
 	});
 
 }
@@ -63,20 +73,20 @@ var data = {}; // instanceId => users
 
 function getAllUsers (worldData) {
 	if (worldData['instances'] !== 'undefined') {
-		
+
 		worldData['instances'].forEach(function(item, i, arr) {
-			
+
 			// get users info
 			xhr.open("GET", "/api/1/worlds/"+worldData['id']+"/"+item[0], false);
 			xhr.onload = function (){
 				data[item[0]] = JSON.parse(xhr.responseText)['users'];
 			}
 			xhr.send(null);
-			
+
 		});
-		
+
 		render(worldData['id']);
-		
+
 	}
 }
 
@@ -87,7 +97,7 @@ var BreakException = {};
 try {
 	Object.keys(data).forEach(function (item){
 			var el = $('a[href="vrchat://launch?ref=vrchat.com&id='+worldId+':'+item+'"]');
-			
+
 			if (el.length){
 				data[item].forEach(function (item,i,arr){
 					el.after('<a href="/home/user/'+item['id']+'" target="_blank" style="display: inline-block;font-size: 12px;width: 130px;text-align: center;background: #333333;border: 1px solid #333333;margin-bottom: 5px;"><img src="'+item['currentAvatarImageUrl']+'">'+item['displayName']+'</a>');
@@ -102,5 +112,5 @@ try {
 } catch (e) {
   if (e !== BreakException) throw e;
 }
-});		
+});
 }
